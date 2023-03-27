8f006ce347c80a102b587643fad76a7d9429bbba
Sourav Mukherjee <mukherjeesourav289@gmail.com>
2023-03-27 22:45:20 +0530

    first

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+import "./header.css";
+
+import {
+  faBed,
+  faCalendar,
+  faCalendarDays,
+  faPerson,
+} from "@fortawesome/free-solid-svg-icons";
+import { faPlane } from "@fortawesome/free-solid-svg-icons";
+import { faCar } from "@fortawesome/free-solid-svg-icons";
+import { faTaxi } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxi</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">A lifetime of Discounts? It's Genius.</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels- unlock intant savings of 10% or more
+          with a free Booking.com account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              className="headerSearchInput"
+              placeholder="Where are you going?"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span className="headerSearchText">date to date</span>
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">2 adults 2 children 1 room</span>
+          </div>
+          <div className="headerSearchItem">
+            <button className="headerBtn">Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
